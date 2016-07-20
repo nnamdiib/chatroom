d321911bf98c9752c442c764efe480ee77f6741a
Nnamdi <nnamdiibns@gmail.com>
2016-07-20 23:45:07 +0100

    Delete useless lines

--- chat.py
+++ chat.py
@@ -202,7 +202,6 @@ def main():
     # app settings
     settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
-    "cookie_secret": "__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
     }
 
     # Creating the app instance
